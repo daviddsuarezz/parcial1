6ad6387bfe83f572ec70ad4d1c6a846355e637c2
daviddsuarezz <dasvandroid@gmail.com>
2023-09-17 20:36:51 -0500

    Nueva estructura

--- Codigo_Fuente.cpp
+++ Codigo_Fuente.cpp
@@ -2,14 +2,17 @@ const int PinData = 10;
 const int PinLatch = 12;
 const int PinClock = 13;
 int ContadorBits;
-
-void LedWrite(int led){
-    shiftOut(PinData, PinClock, LSBFIRST, led);
-}
+int matriz[8][8];
+int arreglo[8];
 
 void store(){
     digitalWrite(PinLatch, HIGH);
     digitalWrite(PinLatch, LOW);
+}
+
+void ledWrite(int led){
+    shiftOut(PinData, PinClock, MSBFIRST, led);
+    store();
 }
 
 void setup(){
@@ -39,15 +42,58 @@ void clearScreen(){
     }
 }
 
-
-
-
-void loop(){
-
+void verificacion(){
     for(int i=0; i<8; ++i){
-        for(int j= 0; j<8; j++){
-            ContadorBits = 1 << j;
-            LedWrite(ContadorBits); delay(100);
-        }
-        store();
+        shiftOut(PinData, PinClock, LSBFIRST, 255);delay(500);
+        digitalWrite(PinLatch, HIGH);
+        digitalWrite(PinLatch, LOW);
     }
+    for(int i=0; i<8; ++i){
+        shiftOut(PinData, PinClock, LSBFIRST, 0);delay(500);
+        digitalWrite(PinLatch, HIGH);
+        digitalWrite(PinLatch, LOW);
+    }
+}
+
+int BitoDecimal(int binario[]){
+    /* Esta funcion convierte un numero binario a decimal. */
+    int decimal = 0;
+    for(int i=7, e=0; i>=0; --i, ++e){
+        decimal = decimal + binario[i]*potencia(2,e);
+    }
+    return decimal;
+}
+
+int potencia(int b, int e){
+    /* Esta funcion calcula la potencia de un numero b elevado a e*/
+    int numero = 1;
+    for(int i=0; i<e; ++i){
+        numero *= b;
+    }
+    return numero;
+}
+
+void patronOn(){
+    /* Esta funcion se encarga de encender los leds correspondientes al
+  patron almacenado en el arreglo 'patron'. */
+    for(int i=7; i>=0; --i){
+        shiftOut(PinData, PinClock, LSBFIRST, BitoDecimal(matriz[i])); delay(100);
+    }
+    digitalWrite(PinLatch, HIGH);
+    digitalWrite(PinLatch, LOW);
+
+};
+
+void setPatron(){
+    /* Esta funcion le asigna a todas las posiciones de 'patron'  el valor
+  de cero. */
+    for(int i=0; i<8; ++i){
+        for(int j=0; j<8; j++){
+            matriz[i][j] = 0;
+        }
+    }
+}
+void loop(){
+    verificacion();
+}
+
